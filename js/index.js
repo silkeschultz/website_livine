273b5f9df1ebfe48ca0ea89c74f437f56093146d
Silke <sisc0003@stud.kea.dk>
2025-10-21 14:27:21 +0200

    Add files

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* const categorylist = document.querySelector(".categorylist");
+
+
+
+fetch("https://dummyjson.com/products/categories")
+    .then(response => response.json())
+    .then(categories => showCategories(categories));
+
+    function showCategories(categories){
+        categories.forEach((category) => {
+            categorylist.innerHTML += `<a  href="productlist.html?category=${category.category}">${category.category}</a>`
+        });
+    }
+ */
